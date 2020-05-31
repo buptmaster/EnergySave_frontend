269493c6306a563964bccc54c61165bfde4f24dc
欧阳宇翔 <768471488@qq.com>
2020-05-31 10:12:38 +0800

    update ui

--- src/pages/Dashboard/components/DeviceOverview/index.jsx
+++ src/pages/Dashboard/components/DeviceOverview/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { Grid, Icon } from '@alifd/next';
 import IceContainer from '@icedesign/container';
 import styles from './index.module.scss';
@@ -7,14 +7,33 @@ import Axios from 'axios';
 const { Row, Col } = Grid;
 export default function DeviceOverview() {
 
+  let intervalRef = useRef()
+
   const [value, setValue] = useState({
     offline: 0,
     allow: 0,
     forbidden: 0
   }) 
+  
+  const [mqtt, setMqtt] = useState({
+    in: '0',
+    out: '0',
+    connected: '0',
+    disconnected: '0',
+    total: '0'
+  })
 
   useEffect(
-    () => {fetchData()}, []
+    () => {
+      fetchData()
+      intervalRef.current = setInterval(() => {
+        fetchStatus()
+      }, 2000)
+
+      return () => {
+        clearInterval(intervalRef.current)
+      }
+    }, []
   )
 
   const fetchData = useCallback(
@@ -23,6 +42,9 @@ export default function DeviceOverview() {
       .then((res) => setValue(res.data.data))
     }
   )
+
+  const fetchStatus = () => { Axios.get('/device/mqtt').then((res) => setMqtt(res.data.data))}
+
 
   return (
     <Row wrap gutter={20} style={{ marginBottom: '20px' }}>
@@ -52,6 +74,56 @@ export default function DeviceOverview() {
                 <p className={styles.title}>禁用设备数</p>
                 <div className={styles.data}>
                   <h2 className={styles.amount}>{value.forbidden}</h2>
+                </div>
+              </div>
+            </IceContainer>
+          </Col>
+          <Col xxs="12" l="4" >
+            <IceContainer className={styles.container}>
+              <div className={styles.content}>
+                <p className={styles.title}>MQTT发送字节数</p>
+                <div className={styles.data}>
+                  <h2 className={styles.amount}>{mqtt.out ? mqtt.out : '0'}</h2>
+                </div>
+              </div>
+            </IceContainer>
+          </Col>
+          <Col xxs="12" l="4" >
+            <IceContainer className={styles.container}>
+              <div className={styles.content}>
+                <p className={styles.title}>MQTT收到字节数</p>
+                <div className={styles.data}>
+                  <h2 className={styles.amount}>{mqtt.in ? mqtt.in : '0'}</h2>
+                </div>
+              </div>
+            </IceContainer>
+          </Col>
+          <Col xxs="12" l="4" >
+            <IceContainer className={styles.container}>
+              <div className={styles.content}>
+                <p className={styles.title}>MQTT已连接总数</p>
+                <div className={styles.data}>
+                  <h2 className={styles.amount}>{mqtt.connected ? mqtt.connected : '0'}</h2>
+                </div>
+              </div>
+            </IceContainer>
+          </Col>
+          <Col xxs="12" l="4" >
+            <IceContainer className={styles.container}>
+              <div className={styles.content}>
+                <p className={styles.title}>MQTT已断开总数</p>
+                <div className={styles.data}>
+                  <h2 className={styles.amount}>{mqtt.disconnected ? mqtt.disconnected :'0'}</h2>
+                </div>
+              </div>
+            </IceContainer>
+          </Col>
+          <Col xxs="12" l="4" >
+            <IceContainer className={styles.container}>
+              <div className={styles.content}>
+                <p className={styles.title}>MQTT总连接数</p>
+                <div className={styles.data}>
+                  <h2 className={styles.amount}>{mqtt.total ? mqtt.total : '0'}</h2>
                 </div>
               </div>
             </IceContainer>
